9847f7bc5318f9362e543511bf13caf41f7d8075
Anupam Tarai <anupam@essbbsr.esspl.com>
2026-06-12 00:03:56 +0530

    test notification

--- src/main/java/com/anupam/reminiscence/service/impl/NotificationScheduler.java
+++ src/main/java/com/anupam/reminiscence/service/impl/NotificationScheduler.java
@@ -79,7 +79,7 @@ public class NotificationScheduler {
             ZonedDateTime nowInZone = ZonedDateTime.now(zoneId);
 
             // Only notify if it's 5:00 AM or later
-            if (nowInZone.toLocalTime().isAfter(LocalTime.of(5, 0)) && nowInZone.toLocalTime().isBefore(LocalTime.of(6, 0))) {
+            if (nowInZone.toLocalTime().isAfter(LocalTime.of(0, 0))) {
 
                 // Check pending reviews for TODAY
                 int pendingCount = userConceptRepo.findPendingReviewsCount(user.getId(), LocalDate.now(zoneId));
@@ -92,7 +92,7 @@ public class NotificationScheduler {
 
                     pushService.sendNotification(
                             user.getPushToken(),
-                            "Morning Review Ready 📚",
+                            "Morning "+pendingCount+" Review Ready 📚",
                             "Good morning " + firstName + "! You have " + pendingCount + " concepts ready for your daily revision.",
                             "/revisions"
                     );
